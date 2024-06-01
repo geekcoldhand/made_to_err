95167d855de393c7ed968a314039cd1621003f58
geekcoldhand <horatiousaharris@gmail.com>
2024-05-31 21:24:04 -0400

    added touch event feature

--- index.js
+++ index.js
@@ -17,26 +17,41 @@ function randomizeDragItemPosition(items) {
 }
 
 function populateBoxesWithDelay(items) {
+//multiple items add to list
+
+
+
   items.forEach((item, index) => {
     setTimeout(() => {
       randomizeDragItemPosition(item);
-    }, index * 200); //lazy load
+    }, index * 200); //staggered load
   });
 }
 
+//start
 populateBoxesWithDelay(dragItems);
-
 const itemStateAndPosition = {};
+
+
+
+
+/**
+ * Touch Event Start
+ */
 dragItems.forEach((item, index) => {
   item.addEventListener("touchstart", (e) => {
+    e.preventDefault(); // Prevent scrolling on touch devices
     itemStateAndPosition[index] = {
       isDragging: true,
-      offsetX: e.clientX - item.offsetLeft,
-      offsetY: e.clientY - item.offsetTop,
+      offsetX: e.touches[0].clientX - item.offsetLeft,
+      offsetY: e.touches[0].clientY - item.offsetTop,
     };
   });
 });
 
+/**
+ * Mouse Event Start
+ */
 dragItems.forEach((item, index) => {
   item.addEventListener("mousedown", (e) => {
     itemStateAndPosition[index] = {
@@ -47,49 +62,33 @@ dragItems.forEach((item, index) => {
   });
 });
 
-// Add event listener for mousemove on the document
-document.addEventListener("mousemove", (e) => {
-  e.preventDefault();
-
-  // Iterate over boxState object and update positions if dragging
-  Object.keys(itemStateAndPosition).forEach((key) => {
-    const state = itemStateAndPosition[key];
-    if (state.isDragging) {
-      const item = dragItems[key];
-      const x = e.clientX - state.offsetX;
-      const y = e.clientY - state.offsetY;
-      const container = document.getElementById("container");
-
-      const maxX = container.offsetWidth - item.offsetWidth;
-      const maxY = container.offsetHeight - item.offsetHeight;
-
-      item.style.left = `${Math.min(Math.max(x, 0), maxX)}px`;
-      item.style.top = `${Math.min(Math.max(y, 0), maxY)}px`;
-    }
-  });
-});
-
-document.addEventListener("mousemove", (e) => {
-  e.preventDefault();
-
-  // Iterate over boxState object and update positions if dragging
-  Object.keys(itemStateAndPosition).forEach((key) => {
-    const state = itemStateAndPosition[key];
-    if (state.isDragging) {
-      const item = dragItems[key];
-      const x = e.clientX - state.offsetX;
-      const y = e.clientY - state.offsetY;
-      const container = document.getElementById("container");
-
-      const maxX = container.offsetWidth - item.offsetWidth;
-      const maxY = container.offsetHeight - item.offsetHeight;
-
-      item.style.left = `${Math.min(Math.max(x, 0), maxX)}px`;
-      item.style.top = `${Math.min(Math.max(y, 0), maxY)}px`;
-    }
-  });
-});
+/**
+ * Touch Move 
+ */
 document.addEventListener("touchmove", (e) => {
+  // Iterate over itemStateAndPosition object and update positions if dragging
+  Object.keys(itemStateAndPosition).forEach((key) => {
+    const state = itemStateAndPosition[key];
+    if (state.isDragging) {
+      e.preventDefault(); // Prevent scrolling on touch devices
+      const item = dragItems[key];
+      const x = e.touches[0].clientX - state.offsetX;
+      const y = e.touches[0].clientY - state.offsetY;
+      const container = document.getElementById("container");
+
+      const maxX = container.offsetWidth - item.offsetWidth;
+      const maxY = container.offsetHeight - item.offsetHeight;
+
+      item.style.left = `${Math.min(Math.max(x, 0), maxX)}px`;
+      item.style.top = `${Math.min(Math.max(y, 0), maxY)}px`;
+    }
+  });
+});
+
+/**
+ * Mouse Move
+ */
+document.addEventListener("mousemove", (e) => {
   e.preventDefault();
 
   // Iterate over boxState object and update positions if dragging
@@ -110,7 +109,9 @@ document.addEventListener("touchmove", (e) => {
   });
 });
 
-// Add event listener for mouseup on the document
+/**
+ * Mouse Up End
+ */
 document.addEventListener("mouseup", () => {
   // Reset dragging state for all boxes
   Object.keys(itemStateAndPosition).forEach((key) => {
@@ -118,6 +119,9 @@ document.addEventListener("mouseup", () => {
   });
 });
 
+/**
+ * Touch End 
+ */
 document.addEventListener("touchend", () => {
   // Reset dragging state for all boxes
   Object.keys(itemStateAndPosition).forEach((key) => {
@@ -125,36 +129,38 @@ document.addEventListener("touchend", () => {
   });
 });
 
+/**
+ * To handle product clicked events; Direct to detail page
+ */
 const handleAddMetaData = async (e) => {
   e.preventDefault();
-
+  console.log(" /TODO if clicked not draged ");
   const productId = e.target?.id;
   const date = Date.now();
   var today = new Date(date);
 
-  const response = await fetch(
-    `http://127.0.0.1:5000/products/${productId}/clicks`,
-    {
-      method: "PUT",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  try {
+    // const response = await fetch(
+    //   `http://127.0.0.1:5000/products/${productId}/clicks`,
+    //   {
+    //     method: "PUT",
+    //     credentials: "include",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
 
-  if (response.ok) {
-    //document.location.replace("/");
-  } else {
-    alert("Failed to create post:::" + response.statusText);
+    // if (response.ok) {
+    //   document.location.replace("/");
+    // } 
+  } catch (error) {
+    console.error(error);
+    alert("Failed to create post:::" + response);
   }
 };
 
-const handleShopProduct = (e) => {
-  console.log("double clicked ");
-};
-
-imageButtonItem.forEach((itemElement) => {
+imageButtonItem.forEach((itemElement, index) => {
   itemElement.addEventListener("click", handleAddMetaData);
-  itemElement.addEventListener("dbclick", handleShopProduct);
 });
+
